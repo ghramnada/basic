5e44860b304bbecd3f75528c1cf87948618c71a7
nada <nada.ghram@enis.tn>
2022-10-31 16:40:58 +0100

    f

---
+++ JGL_MeshLoader/source/elems/Model.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+class Model
+{
+public:
+    Model(char* path)
+    {
+        loadModel(path);
+    }
+    void Draw(Shader& shader);
+private:
+    // model data
+    vector<Mesh> meshes;
+    string directory;
+
+    void loadModel(string path);
+    void processNode(aiNode* node, const aiScene* scene);
+    Mesh processMesh(aiMesh* mesh, const aiScene* scene);
+    vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type,
+        string typeName);
+};
